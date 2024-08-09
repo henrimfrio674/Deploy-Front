32074930bc8a2826f6f030042393d6f6159d8339
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-08-09 10:27:16 -0300

    cadastro e logica do cadastro de usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import UserProvider from './contexts/UserContext';
+import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './Pages/login/Login';
 import Home from './Pages/home/Home';
+import Cadastro from './Pages/cadastro/Cadastro';
+import Login from './Pages/login/Login';
 
 
 
 function App() {
-
   return (
-    <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-
-      </BrowserRouter>
-
-    </UserProvider>
+    <>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
+    </>
   );
 }
-
 export default App;
