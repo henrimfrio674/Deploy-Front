67b8aca9f21be09027863c8c0875ed4f0f060ed3
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-08-12 12:47:33 -0300

    modificação da tailwnd.config.js e outros

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Pages/home/Home';
 import Cadastro from './Pages/cadastro/Cadastro';
 import Login from './Pages/login/Login';
-import { AuthProvider } from './contexts/AuthContext';
+import Home from './Pages/home/Home';
+
 
 
 function App() {
   return (
     <>
-       <AuthProvider>
+   
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
@@ -26,7 +26,6 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider> 
   
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
   plugins: [],
 }
