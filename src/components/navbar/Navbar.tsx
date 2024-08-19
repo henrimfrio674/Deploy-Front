229c2007494514dd9dfbc63e811c2641d8244dd6
henrimfrio674 <henri.felix.araujo@gmail.com>
2024-08-19 16:42:48 -0300

    jhuvgihgvhu

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-import homeLogo from '../../assets/naturezaNav.jpg'
 import { toastAlerta } from '../../util/toastAlerta'
 import { useContext } from 'react'
 
